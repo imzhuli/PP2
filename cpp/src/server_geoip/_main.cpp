3c9d764b7933517d73f520549f1ef176bfa1970c
zhuli <imzhuli@vip.qq.com>
2024-03-24 12:59:25 +0800

    test geo service

--- cpp/src/server_geoip/_main.cpp
+++ cpp/src/server_geoip/_main.cpp
@@ -1,8 +1,13 @@
 #include "../../pb/pp2/geo.pb.h"
 #include "../common/base.hpp"
+#include "../common_protocol/protocol.hpp"
+#include "../component/geo_ip.hpp"
 
 #include <core/core_min.hpp>
 #include <server_arch/client.hpp>
+
+static auto HW = "Hello world!";
+static auto GS = xGeoService{};
 
 class xGeoIpConsumer : public xClient {
 public:
@@ -20,16 +25,77 @@ public:
 		PostData(Buffer, RSize);
 	}
 
-	std::vector<xPacketCommandId> InterestedCommandIds = { 0x01 };
-};
+	bool OnPacket(const xPacketHeader & Header, ubyte * PayloadPtr, size_t PayloadSize) {
+		switch (Header.CommandId) {
+			case Cmd_GeoQuery:
+				OnGeoQuery(Header, PayloadPtr, PayloadSize);
+				break;
+			default:
+				break;
+		}
+		return true;
+	}
 
-class xGeoIpUdpServer : xUdpService {
-protected:
-	void OnPacket(const xNetAddress & RemoteAddress, const xPacketHeader & Header, ubyte * PayloadPtr, size_t PayloadSize) override {
+	void OnGeoQuery(const xPacketHeader & Header, ubyte * PayloadPtr, size_t PayloadSize) {
 		auto Req = geo::xGeoInfoReq();
 		if (!Req.ParseFromArray(PayloadPtr, PayloadSize)) {
 			return;
 		}
+		if (Req.hello_world() != HW) {
+			return;
+		}
+		auto RA = xNetAddress::Parse(Req.ip());
+		auto AS = RA.IpToString();
+		auto GI = GS.GetRegionId(AS.c_str());
+		X_DEBUG_PRINTF("GI: \n%s", HexShow(&GI.CountryId, sizeof(GI.CountryId)).c_str());
+
+		auto Resp = geo::xGeoInfoResp();
+		Resp.set_ip(AS);
+		Resp.set_country_id(GI.CountryId);
+		Resp.set_city_id(GI.CityId);
+		ubyte  RespBuffer[MaxPacketSize];
+		size_t RespSize = PbWritePacket(Cmd_GeoQueryResp, Header.RequestId, RespBuffer, sizeof(RespBuffer), Resp);
+
+		PostData(RespBuffer, RespSize);
+	}
+
+	std::vector<xPacketCommandId> InterestedCommandIds = { 0x01 };
+};
+
+class xGeoIpUdpServer : public xUdpService {
+protected:
+	void OnPacket(const xNetAddress & RemoteAddress, const xPacketHeader & Header, ubyte * PayloadPtr, size_t PayloadSize) override {
+		if (Header.CommandId != Cmd_GeoQuery) {
+			return;
+		}
+		X_DEBUG_PRINTF("OnPacket: from:%s \n%s", RemoteAddress.ToString().c_str(), HexShow(PayloadPtr, PayloadSize).c_str());
+		auto Req = geo::xGeoInfoReq();
+		if (!Req.ParseFromArray(PayloadPtr, PayloadSize)) {
+			return;
+		}
+		if (Req.hello_world() != HW) {
+			return;
+		}
+		auto RA = xNetAddress::Parse(Req.ip());
+		if (RA) {
+			X_DEBUG_PRINTF("QueryByRequest: %s", RA.IpToString().c_str());
+		} else {
+			RA = RemoteAddress;
+			X_DEBUG_PRINTF("QueryBySource: %s", RA.IpToString().c_str());
+		}
+		auto AS = RA.IpToString();
+		auto GI = GS.GetRegionId(AS.c_str());
+		X_DEBUG_PRINTF("GI: \n%s", HexShow(&GI.CountryId, sizeof(GI.CountryId)).c_str());
+
+		auto Resp = geo::xGeoInfoResp();
+		Resp.set_ip(AS);
+		Resp.set_country_id(GI.CountryId);
+		Resp.set_city_id(GI.CityId);
+		ubyte  RespBuffer[MaxPacketSize];
+		size_t RespSize = PbWritePacket(Cmd_GeoQueryResp, Header.RequestId, RespBuffer, sizeof(RespBuffer), Resp);
+
+		X_DEBUG_PRINTF("PostData: %s\n%s", RemoteAddress.ToString().c_str(), HexShow(RespBuffer, RespSize).c_str());
+		PostData(RespBuffer, RespSize, RemoteAddress);
 	}
 };
 
@@ -43,18 +109,35 @@ int main(int argc, char ** argv) {
 		argc, argv,
 		{
 			{ 'd', nullptr, "dispatcher_consumer", true },
+			{ 'u', nullptr, "udp_bind_address", true },
+			{ 'f', nullptr, "geo_filename", true },
 		}
 	);
 
+	auto GF   = "./test_assets/gl2-city.mmdb";
+	auto OptG = CL["geo_filename"];
+	if (OptG()) {
+		GF = OptG->c_str();
+	}
+	auto GG = xResourceGuard(GS, GF);
+	assert(GG);
+
 	auto OptD = CL["dispatcher_consumer"];
+	if (!OptD()) {
+		cerr << "invalid usage, need dispatcher address" << endl;
+		return -1;
+	}
+	auto OptU = CL["udp_bind_address"];
 	if (!OptD()) {
 		cerr << "invalid usage" << endl;
 		return -1;
 	}
 
-	auto BAddress = xNetAddress::Parse(OptD->c_str());
-	auto IG       = xResourceGuard(IoCtx);
-	auto OG       = xResourceGuard(Consumer, &IoCtx, BAddress);
+	auto BAddress  = xNetAddress::Parse(OptD->c_str());
+	auto UBAddress = xNetAddress::Parse(OptU->c_str());
+	auto IG        = xResourceGuard(IoCtx);
+	auto OG        = xResourceGuard(Consumer, &IoCtx, BAddress);
+	auto UG        = xResourceGuard(UdpServer, &IoCtx, UBAddress);
 
 	auto T = xTicker();
 	while (true) {
---
+++ cpp/src/test_geoip/main.cpp
@@ -0,0 +1,117 @@
+#include "../../pb/pp2/geo.pb.h"
+#include "../common/base.hpp"
+#include "../common_protocol/protocol.hpp"
+
+#include <core/core_min.hpp>
+#include <server_arch/client.hpp>
+
+using namespace std;
+using namespace xel;
+
+class xTester : public xUdpChannel::iListener {
+public:
+	bool Init(const xNetAddress & TargetAddress) {
+		if (!IoCtx.Init()) {
+			return false;
+		}
+		auto IOC = MakeResourceCleaner(IoCtx);
+
+		if (!Channel.Init(&IoCtx, AF_INET, this)) {
+			return false;
+		}
+		auto CC = MakeResourceCleaner(Channel);
+
+		this->TargetAddress = TargetAddress;
+
+		Dismiss(IOC, CC);
+		return true;
+	}
+
+	void Clean() {
+		auto RCS = MakeResourceCleaner(IoCtx);
+	}
+
+	xIoContext * GetIoCtx() {
+		return &IoCtx;
+	}
+
+	bool Sent = false;
+	void Tick() {
+		Ticker.Update();
+		IoCtx.LoopOnce();
+		if (!Steal(Sent, true)) {
+			auto R = geo::xGeoInfoReq();
+			R.set_ip("14.153.209.68");
+			R.set_hello_world("Hello world!");
+
+			ubyte  Buffer[MaxPacketSize];
+			size_t RSize = PbWritePacket(Cmd_GeoQuery, 1, Buffer, sizeof(Buffer), R);
+
+			cout << "Post: " << TargetAddress.ToString() << endl;
+			cout << HexShow(Buffer, RSize) << endl;
+			Channel.PostData(Buffer, RSize, TargetAddress);
+		}
+	}
+	uint64_t GetTimestamp() {
+		return Ticker;
+	}
+
+protected:
+	void OnData(xUdpChannel * ChannelPtr, void * DataPtr, size_t DataSize, const xNetAddress & RemoteAddress) {
+		cout << "UdpFrom: " << RemoteAddress.ToString() << endl;
+		cout << HexShow(DataPtr, DataSize) << endl;
+	}
+
+private:
+	xIoContext  IoCtx   = {};
+	xUdpChannel Channel = {};
+	xTicker     Ticker  = {};
+
+	xNetAddress TargetAddress = {};
+};
+
+class xTcpTester : public xClient {
+public:
+	void OnServerConnected() {
+		auto R = geo::xGeoInfoReq();
+		R.set_ip("14.153.209.68");
+		R.set_hello_world("Hello world!");
+
+		ubyte  Buffer[MaxPacketSize];
+		size_t RSize = PbWritePacket(Cmd_GeoQuery, 1, Buffer, sizeof(Buffer), R);
+
+		cout << HexShow(Buffer, RSize) << endl;
+		PostData(Buffer, RSize);
+	}
+};
+
+auto R       = xRunState();
+auto Tester  = xTester();
+auto TTester = xTcpTester();
+
+int main(int argc, char ** argv) {
+
+	auto CL = xCommandLine(
+		argc, argv,
+		{
+			{ 'u', nullptr, "udp_server", true },
+			{ 'd', nullptr, "dispatcher_producer", true },
+		}
+	);
+
+	auto OptD = CL["dispatcher_producer"];
+	RuntimeAssert(OptD());
+
+	auto OptU = CL["udp_server"];
+	RuntimeAssert(OptU());
+
+	auto TG  = xResourceGuard(Tester, xNetAddress::Parse(*OptU));
+	auto TTG = xResourceGuard(TTester, Tester.GetIoCtx(), xNetAddress::Parse(*OptD));
+
+	R.Start();
+	while (R) {
+		Tester.Tick();
+		TTester.Tick(Tester.GetTimestamp());
+	}
+	R.Finish();
+}
