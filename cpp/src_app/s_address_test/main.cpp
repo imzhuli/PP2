5125de13cfdb425b60829f1d598bcb0f37eada75
imzhuli <imzhuli@vip.qq.com>
2026-01-22 17:25:56 +0800

    clean up

--- cpp/src_app/s_address_test/main.cpp
+++ cpp/src_app/s_address_test/main.cpp
@@ -2,14 +2,6 @@
 #include <pp_protocol/command.hpp>
 #include <pp_protocol/device/check_address.hpp>
 
-static void OnTcpClientConnected(const xTcpServiceClientConnectionHandle & Handle) {
-    auto PeerAddress = Handle.GetRemoteAddress();
-    Logger->I("NewConnection: Id=%" PRIx64 ", Address=%s", Handle.GetConnectionId(), PeerAddress.ToString().c_str());
-    auto Push              = xPP_AddressAutoPush();
-    Push.ConnectionAddress = PeerAddress.Ip();
-    Handle.PostMessage(Cmd_DV_RL_AddressPush, 0, Push);
-}
-
 static void OnUdpPacket(const xUdpServiceChannelHandle & Handle, xPacketCommandId CommandId, xPacketRequestId, ubyte *, size_t) {
     if (CommandId != Cmd_DV_RL_AddressChallenge) {
         return;
@@ -19,8 +11,6 @@ static void OnUdpPacket(const xUdpServiceChannelHandle & Handle, xPacketCommandI
     Handle.PostMessage(Cmd_DV_RL_AddressPush, 0, Push);
 }
 
-static xTcpService TestAddressServer4;
-static xTcpService TestAddressServer6;
 static xUdpService TestAddressUdpServer4;
 static xUdpService TestAddressUdpServer6;
 
@@ -41,25 +31,14 @@ int main(int argc, char ** argv) {
         AuditLogger->F("no available bind address");
     }
 
-    TestAddressServer4.OnClientConnected = OnTcpClientConnected;
-    TestAddressServer6.OnClientConnected = OnTcpClientConnected;
-
     TestAddressUdpServer4.OnPacketCallback = OnUdpPacket;
     TestAddressUdpServer6.OnPacketCallback = OnUdpPacket;
 
-    X_COND_GUARD(V4Enabled, TestAddressServer4, ServiceIoContext, BindAddress4, 1024);
     X_COND_GUARD(V4Enabled, TestAddressUdpServer4, ServiceIoContext, BindAddress4);
-    X_COND_GUARD(V6Enabled, TestAddressServer6, ServiceIoContext, BindAddress6, 1024);
     X_COND_GUARD(V6Enabled, TestAddressUdpServer6, ServiceIoContext, BindAddress6);
 
     while (ServiceRunState) {
         ServiceUpdateOnce();
-        if (V4Enabled) {
-            TestAddressServer4.Tick(ServiceTicker());
-        }
-        if (V6Enabled) {
-            TestAddressServer6.Tick(ServiceTicker());
-        }
     }
 
     return 0;
--- cpp/src_lib_protocol/pp_protocol/command.hpp
+++ cpp/src_lib_protocol/pp_protocol/command.hpp
@@ -1,42 +1,6 @@
 #pragma once
 #include <pp_common/_.hpp>
 
-// static constexpr const xPacketCommandId CmdRespBase = xel::MaxDispatchableCommandIdCount;
-
-// static constexpr const xPacketCommandId Cmd_GeoQuery            = 0x01;
-// static constexpr const xPacketCommandId Cmd_HostQuery           = 0x02;
-// static constexpr const xPacketCommandId Cmd_GetTerminalWithAuth = 0x03;
-// static constexpr const xPacketCommandId Cmd_ReportAccountUsage  = 0x04;
-// static constexpr const xPacketCommandId Cmd_ProxyClientAuth     = 0x05;
-
-// static constexpr const xPacketCommandId Cmd_GeoQueryResp            = CmdRespBase + Cmd_GeoQuery;
-// static constexpr const xPacketCommandId Cmd_HostQueryResp           = CmdRespBase + Cmd_HostQuery;
-// static constexpr const xPacketCommandId Cmd_GetTerminalWithAuthResp = CmdRespBase + Cmd_GetTerminalWithAuth;
-// static constexpr const xPacketCommandId Cmd_ReportAccountUsageResp  = CmdRespBase + Cmd_ReportAccountUsage;  // not used
-// static constexpr const xPacketCommandId Cmd_ProxyClientAuthResp     = CmdRespBase + Cmd_ProxyClientAuth;
-
-// non dispatchable or direct command
-static constexpr const xPacketCommandId CmdProxyBase = 0x02'000;
-// static constexpr const xPacketCommandId Cmd_CreateConnection         = CmdProxyBase + 0x01;
-// static constexpr const xPacketCommandId Cmd_CreateConnectionResp     = CmdProxyBase + 0x02;
-// static constexpr const xPacketCommandId Cmd_CloseConnection          = CmdProxyBase + 0x03;
-// static constexpr const xPacketCommandId Cmd_PostProxyToRelayData     = CmdProxyBase + 0x04;
-// static constexpr const xPacketCommandId Cmd_PostRelayToProxyData     = CmdProxyBase + 0x05;
-// static constexpr const xPacketCommandId Cmd_CreateUdpAssociation     = CmdProxyBase + 0x06;
-// static constexpr const xPacketCommandId Cmd_CreateUdpAssociationResp = CmdProxyBase + 0x07;
-// static constexpr const xPacketCommandId Cmd_CloseUdpAssociation      = CmdProxyBase + 0x08;
-// static constexpr const xPacketCommandId Cmd_PostProxyToRelayUdpData  = CmdProxyBase + 0x09;
-// static constexpr const xPacketCommandId Cmd_PostRelayToProxyUdpData  = CmdProxyBase + 0x0A;
-
-// management
-// static constexpr const xPacketCommandId Cmd_ManagementBase       = 0x02'100;
-// static constexpr const xPacketCommandId Cmd_PostError            = Cmd_ManagementBase + 0x01;
-// static constexpr const xPacketCommandId Cmd_PostErrorResp        = Cmd_ManagementBase + 0x02;
-// static constexpr const xPacketCommandId Cmd_PostTerminalAudit    = Cmd_ManagementBase + 0x03;
-// static constexpr const xPacketCommandId Cmd_PostRelayServerAudit = Cmd_ManagementBase + 0x04;
-
-// version 2024 from here
-
 // backend base:
 static constexpr const xPacketCommandId Cmd_BackendBase           = 0x04'000;
 static constexpr const xPacketCommandId Cmd_AuditTerminalInfo     = Cmd_BackendBase + 0x01;  // 旧版, 弃用
@@ -56,48 +20,12 @@ static constexpr const xPacketCommandId Cmd_BackendAuthByUserPassResp = Cmd_Back
 
 // server management:
 static constexpr const xPacketCommandId Cmd_ServerManagementBase    = 0x05'000;
-static constexpr const xPacketCommandId Cmd_QueryRunningState       = Cmd_ServerManagementBase + 1;
-static constexpr const xPacketCommandId Cmd_QueryRunningStateResp   = Cmd_ServerManagementBase + 2;
-static constexpr const xPacketCommandId Cmd_RegisterDnsServer       = Cmd_ServerManagementBase + 3;  // get dns server group id from config center
-static constexpr const xPacketCommandId Cmd_RegisterDnsServerResp   = Cmd_ServerManagementBase + 4;
-static constexpr const xPacketCommandId Cmd_CheckinDnsServer        = Cmd_ServerManagementBase + 5;  // check into dispatcher with group id from config center
-static constexpr const xPacketCommandId Cmd_CheckinDnsServerResp    = Cmd_ServerManagementBase + 6;
-static constexpr const xPacketCommandId Cmd_EnableDnsDispatcher     = Cmd_ServerManagementBase + 7;
-static constexpr const xPacketCommandId Cmd_EnableDnsDispatcherResp = Cmd_ServerManagementBase + 8;
-
 static constexpr const xPacketCommandId Cmd_AcquireServerId         = Cmd_ServerManagementBase + 0x101;
 static constexpr const xPacketCommandId Cmd_AcquireServerIdResp     = Cmd_ServerManagementBase + 0x102;
 static constexpr const xPacketCommandId Cmd_RegisterService         = Cmd_ServerManagementBase + 0x103;
 static constexpr const xPacketCommandId Cmd_RegisterServiceResp     = Cmd_ServerManagementBase + 0x104;
 static constexpr const xPacketCommandId Cmd_DownloadServiceList     = Cmd_ServerManagementBase + 0x105;
 static constexpr const xPacketCommandId Cmd_DownloadServiceListResp = Cmd_ServerManagementBase + 0x106;
-
-///////////////////////// PPPro server list
-static constexpr const xPacketCommandId Cmd_RegisterAuthCacheServer                        = Cmd_ServerManagementBase + 0x201;
-static constexpr const xPacketCommandId Cmd_RegisterAuditDeviceServer                      = Cmd_ServerManagementBase + 0x202;
-static constexpr const xPacketCommandId Cmd_RegisterAuditAccountServer                     = Cmd_ServerManagementBase + 0x203;
-static constexpr const xPacketCommandId Cmd_RegisterDeviceStateRelayServer                 = Cmd_ServerManagementBase + 0x204;
-static constexpr const xPacketCommandId Cmd_RegisterRelayInfoDispatcherServer              = Cmd_ServerManagementBase + 0x205;
-static constexpr const xPacketCommandId Cmd_RegisterDeviceSelectorDispatcherServer         = Cmd_ServerManagementBase + 0x206;
-static constexpr const xPacketCommandId Cmd_RegisterDeviceSelector                         = Cmd_ServerManagementBase + 0x207;
-static constexpr const xPacketCommandId Cmd_RegisterAuditTargetServer                      = Cmd_ServerManagementBase + 0x208;
-static constexpr const xPacketCommandId Cmd_RegisterServerResp                             = Cmd_ServerManagementBase + 0x2FF;
-static constexpr const xPacketCommandId Cmd_DownloadAuthCacheServerList                    = Cmd_ServerManagementBase + 0x301;
-static constexpr const xPacketCommandId Cmd_DownloadAuthCacheServerListResp                = Cmd_ServerManagementBase + 0x302;
-static constexpr const xPacketCommandId Cmd_DownloadAuditDeviceServerList                  = Cmd_ServerManagementBase + 0x303;
-static constexpr const xPacketCommandId Cmd_DownloadAuditDeviceServerListResp              = Cmd_ServerManagementBase + 0x304;
-static constexpr const xPacketCommandId Cmd_DownloadAuditAccountServerList                 = Cmd_ServerManagementBase + 0x305;
-static constexpr const xPacketCommandId Cmd_DownloadAuditAccountServerListResp             = Cmd_ServerManagementBase + 0x306;
-static constexpr const xPacketCommandId Cmd_DownloadDeviceStateRelayServerList             = Cmd_ServerManagementBase + 0x307;
-static constexpr const xPacketCommandId Cmd_DownloadDeviceStateRelayServerListResp         = Cmd_ServerManagementBase + 0x308;
-static constexpr const xPacketCommandId Cmd_DownloadBackendServerList                      = Cmd_ServerManagementBase + 0x309;
-static constexpr const xPacketCommandId Cmd_DownloadBackendServerListResp                  = Cmd_ServerManagementBase + 0x30A;
-static constexpr const xPacketCommandId Cmd_DownloadRelayInfoDispatcherServer              = Cmd_ServerManagementBase + 0x30B;
-static constexpr const xPacketCommandId Cmd_DownloadRelayInfoDispatcherServerResp          = Cmd_ServerManagementBase + 0x30C;
-static constexpr const xPacketCommandId Cmd_DownloadDeviceSelectorDispatcherServerList     = Cmd_ServerManagementBase + 0x30D;
-static constexpr const xPacketCommandId Cmd_DownloadDeviceSelectorDispatcherServerListResp = Cmd_ServerManagementBase + 0x30E;
-static constexpr const xPacketCommandId Cmd_DownloadAuditTargetServerList                  = Cmd_ServerManagementBase + 0x30F;
-static constexpr const xPacketCommandId Cmd_DownloadAuditTargetServerListResp              = Cmd_ServerManagementBase + 0x310;
 
 ///////////////////////// PP2 server list
 
@@ -158,14 +86,10 @@ static constexpr const xPacketCommandId Cmd_DSR_DS_Base         = 0x09'000;
 static constexpr const xPacketCommandId Cmd_DSR_DS_DeviceUpdate = Cmd_DSR_DS_Base + 0x01;
 
 // device-selector
-static constexpr const xPacketCommandId Cmd_DeviceSelector_Base              = 0x0D'000;
-static constexpr const xPacketCommandId Cmd_DeviceSelector_AcquireDevice     = Cmd_DeviceSelector_Base + 0x01;
-static constexpr const xPacketCommandId Cmd_DeviceSelector_AcquireDeviceResp = Cmd_DeviceSelector_Base + 0x02;
+static constexpr const xPacketCommandId Cmd_DeviceSelector_Base = 0x0D'000;
 
 // auth-service
-static constexpr const xPacketCommandId Cmd_AuthService_Base               = 0x0E'000;
-static constexpr const xPacketCommandId Cmd_AuthService_QueryAuthCache     = Cmd_AuthService_Base + 0x01;
-static constexpr const xPacketCommandId Cmd_AuthService_QueryAuthCacheResp = Cmd_AuthService_Base + 0x02;
+static constexpr const xPacketCommandId Cmd_AuthService_Base = 0x0E'000;
 
 std::vector<ubyte> Encrypt(const void * Data, size_t DataSize, const std::string & AesKey);
 std::vector<ubyte> Decrypt(const void * Data, size_t DataSize, const std::string & AesKey);
